08f3c35404babc4ad0b4a08eb770e9fdfc18e4f8
daneng1 <danengel@dans-mbp-2.lan>
2022-01-07 15:51:37 -0800

    added title modal to gifs

--- components/gif/index.js
+++ components/gif/index.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Card = styled.div`
   height: 43vh;
-  flex-direction: column;
+  position: relative;
+//   flex-direction: column;
   justify-content: center;
   margin: 5px;
   border-radius: 5px;
@@ -16,19 +17,41 @@ const Image = styled.img`
   border-radius: 5px;
 `;
 
+const InfoWrapper = styled.div`
+    // height: 20vh;
+    position: absolute;
+    margin: 0 auto;
+    background-color: rgba(255,255,255,.75)
+`
+
+const Title = styled.p`
+    font-size: 28px;
+`
+
 export default function Gif({ gif }) {
+  const [viewModal, setViewModal] = useState(false);
   const height = +gif.images.original.height;
   const width = +gif.images.original.width;
 
   return (
     <Card>
       <label>
-        {gif.title}
+        {/* {gif.title} */}
+        {viewModal && 
+            <InfoWrapper>
+                <Title>{gif.title}</Title>
+            </InfoWrapper>
+
+        }
+            <a target="_blank" href={gif.url}>
         <Image
+        onMouseEnter={() => setViewModal(true)}
+        onMouseLeave={() => setViewModal(false)}
           orientation={width >= height ? "horizontal" : "vertical"}
           src={gif.images.original.url}
           alt={gif.title}
         />
+            </a>
       </label>
     </Card>
   );
